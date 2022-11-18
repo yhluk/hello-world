e27ad21fd04f59465e190b6c4b1aa8c6bf50770d
yurii hlukhyi <yhlukhyi@gmail.com>
2022-11-17 17:00:50 -0800

    added css

---
+++ script.js
@@ -0,0 +1,35 @@
+function hello() {
+    let userName = prompt("Hello Rider, who are you?")
+    while (userName == "") {
+        userName = prompt("You got one more chance");
+    }
+    console.log("Rider's name is: " + userName);
+    document.write("Rider " + userName);
+    return userName;
+}
+    
+hello(
+
+);
+
+
+
+let response = prompt("Do you promise to ride safe?");
+if (response == "Yes") {
+    alert("Good Job");
+    document.write(" You promised to be SAFE");
+} else if (response == "No") {
+    alert("That's TERRIBLE CHOISE");
+    document.write(" Very DANGEROUS :)");
+} else {
+    alert("Try again!!!");
+    document.write("Has not decided about");
+}
+
+
+function bikeLover(){
+    let rating = prompt("How much do you like biking from 1 to 10");
+    for (let i = 0; i < rating; i++){
+      document.write("<img class='rating-pic' src='./img/wheel.jpg' alt='Wheeeelee' />");
+    }
+  }
